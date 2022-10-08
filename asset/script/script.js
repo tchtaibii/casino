0e1f2299af8967803260a9153f14ad69f5ec9277
Taha Chtaibi <tchtaibi@e2r5p6.1337.ma>
2022-10-08 23:13:16 +0100

    registre + login

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,4 +1,18 @@
 function registre_login(id) {
+    if (id == 'x')
+    {
+        var y = document.getElementById('panel');
+        var z = document.getElementsByTagName('body');
+        var x = document.getElementById(id);
+        var x2 = document.getElementById(id);
+        z[0].style.overflow = "visible";
+        y.style.display = "none";
+        x.style.display = "none";
+        x1.style.display = "none";
+        var f = document.getElementById(id);
+        f.style.display = "hide";
+
+    }
     var y = document.getElementById('panel');
     var z = document.getElementsByTagName('body');
     z[0].style.overflow = "hidden";
@@ -8,11 +22,15 @@ function registre_login(id) {
         console.log('register');
         var x = document.getElementById(id);
         x.style.display = "flex";
+        var f = document.getElementById('x');
+        f.style.display = "block";
     }
     else if (id == 'registre-panel')
     {
         console.log('register');
         var x = document.getElementById(id);
         x.style.display = "flex";
+        var f = document.getElementById('x');
+        f.style.display = "block";
     }
 }
